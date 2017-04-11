41dc6cc593e4aa0658aab5f75214a349c908abb3
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-04-11 14:31:56 -0600

    PostListTableViewController and Views added

---
+++ Post/PostListTableViewController.swift
@@ -0,0 +1,111 @@
+//
+//  PostListTableViewController.swift
+//  Post
+//
+//  Created by Bradley GIlmore on 4/11/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class PostListTableViewController: UITableViewController,PostControllerDelegate {
+    
+    let postController = PostController()
+    
+    
+    @IBAction func refreshControllerPulled(_ sender: Any) {
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+        postController.fetchPosts(completion: { (newPosts) in
+            self.refreshControl?.endRefreshing()
+            //MARK: - Is this right?
+            
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        })
+        
+        
+        
+    }
+    
+    
+    
+    func postsWereUpdatedTo(posts: [Post], on postController: PostController) {
+        tableView.reloadData()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        postController.delegate = self
+        
+        tableView.estimatedRowHeight = 80
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return postController.posts.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        let post = postController.posts[indexPath.row]
+        
+        cell.textLabel?.text = post.text
+        cell.detailTextLabel?.text = "Author: \(post.username) \n Time: \(post.timestamp)"
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
